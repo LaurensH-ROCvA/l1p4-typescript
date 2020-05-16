383b8988f0ea426c1ad7da89f9c3dc770d799930
LaurensH-ROCvA <laurenshazewinkel@gmail.com>
2020-05-16 17:10:17 +0200

    opgave 10

--- main.ts
+++ main.ts
@@ -3,3 +3,6 @@ let umut: Persoon = new Persoon('Umut', 18, 'M');
 let demirel: Persoon = new Persoon('Demirel', 19, 'M');
 let laurens: Persoon = new Persoon('Laurens', 19, 'M');
 let thamara: Persoon = new Persoon('Thamara', 18, 'V');
+
+demirel.setLeeftijd(24);
+console.log('De leeftijd van Demirel is: ' + demirel.getLeeftijd());
