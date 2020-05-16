09f3db3f5f299969a3de7cf93038dff991241cda
LaurensH-ROCvA <laurenshazewinkel@gmail.com>
2020-05-16 16:45:35 +0200

    opgave 6

--- Persoon.ts
+++ Persoon.ts
@@ -4,4 +4,13 @@ export class Persoon {
     protected geslacht: string;
     public isStudent: boolean;
     public gemiddeldCijfer: number;
+
+    // constructor-methode
+    constructor (naam: string, leeftijd: number, geslacht: string) {
+        this.naam = naam;
+        this.leeftijd = leeftijd;
+        this.geslacht = geslacht;
+        console.log('Nieuw Persoon-object aangemaakt.');
+        console.log('De property naam is ' + this.naam);
+    }
 }
