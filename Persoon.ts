d2db3b999fc13eaaab610f807d9afcf1851dd34c
LaurensH-ROCvA <laurenshazewinkel@gmail.com>
2020-05-16 16:40:36 +0200

    opgave 5

---
+++ Persoon.ts
@@ -0,0 +1,7 @@
+export class Persoon {
+    public naam: string;
+    private leeftijd: number;
+    protected geslacht: string;
+    public isStudent: boolean;
+    public gemiddeldCijfer: number;
+}
