e047b499023055d4ce3739bcf055e17f9b6f70bc
LaurensH-ROCvA <laurenshazewinkel@gmail.com>
2020-05-16 17:11:18 +0200

    lab 02

--- main.ts
+++ main.ts
@@ -6,3 +6,6 @@ let thamara: Persoon = new Persoon('Thamara', 18, 'V');
 
 demirel.setLeeftijd(24);
 console.log('De leeftijd van Demirel is: ' + demirel.getLeeftijd());
+
+thamara.setLeeftijd(19);
+console.log('De leeftijd van Thamara is: ' + thamara.getLeeftijd());
